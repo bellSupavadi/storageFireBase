c46329856c7294812b14ba977cb66362c417f6e1
supavadi <supavadi_bell@kjst.ac.th>
2018-12-18 02:23:17 +0700

    hom Regis

--- scrip.js
+++ scrip.js
@@ -19,7 +19,7 @@ db.settings({
   
 var storage = firebase.storage();
   var pic =''
-  
+ 
   //function to save file
   function previewFile() {
     
@@ -30,7 +30,7 @@ var storage = firebase.storage();
     var storageRef = firebase.storage().ref();
   
     //dynamically set reference to the file name
-    var thisRef = storageRef.child('photo/'+file.name);
+    var thisRef = storageRef.child('license/'+file.name);
   console.log(thisRef);
   
     //put request upload file to firebase storage
@@ -42,8 +42,8 @@ var storage = firebase.storage();
         console.log(url);
         pic=url
 
-add(url)
-        
+
+        add(pic)
 
       }).catch(function(error) {
       
@@ -74,17 +74,35 @@ add(url)
    
   
   }
+  
 
   function add(pic){
+    var typecar = document.getElementById('typecar').value
+    var typepeplo = document.getElementById('typepeplo').value
+    var name = document.getElementById('name').value
+    var lastname = document.getElementById('lastname').value
+    var stuid = document.getElementById('stuid').value
+    var collect = document.getElementById('collect').value
+    var major = document.getElementById('major').value
+    var year = document.getElementById('year').value
+    var carnumber = document.getElementById('carnumber').value
+    var provide = document.getElementById('provide').value
+    var brand = document.getElementById('brand').value
+    var email = document.getElementById('email').value
+console.log(typecar);
+
+    
     console.log(pic);
     
     var img=pic;
-    var obj = JSON.parse('{ "name":"'+img+'"}');
+    var obj = JSON.parse('{ "img":"'+img+'","typecar":"'+typecar+'","typepeplo":"'+typepeplo+'","name":"'+name+'","lastname":"'+lastname+'","stuid":"'+stuid+'","collect":"'+collect+'","major":"'+major+'","year":"'+year+'","carnumber":"'+carnumber+'","provide":"'+provide+'","brand":"'+brand+'","email":"'+email+'"}');
 console.log(obj);
+
 
 db.collection("user").add(obj)
 .then(function(docRef) {
     console.log("Document written with ID: ", docRef.id);
+    location.reload();
 })
 .catch(function(error) {
     console.error("Error adding document: ", error);
